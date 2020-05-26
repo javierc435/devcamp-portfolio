5658f1cfd3530e006426f51b633b24532fd1fee5
Javier Cuadrado Manteca <mantecajc@gmail.com>
2020-05-26 14:06:53 +0200

    Updated blogs controller and routes

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -52,9 +52,20 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Post elminado' }
+      format.html { redirect_to blogs_url, notice: 'Post suprimido.' }
       format.json { head :no_content }
     end
+  end
+
+  def toggle_status
+  
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+      
+    redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
   private
