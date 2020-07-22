439a7b8aed1ec5face6cce4538e14b7ff0485829
Javier Cuadrado Manteca <mantecajc@gmail.com>
2020-07-22 16:32:02 +0200

    Integrated initial drag and drop implementation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,10 +9,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery')
-require('jquery-ui')
 
-require("packs/html.sortable")
+require('jquery')
+require("jquery-ui")
+
+$(function(){
+  $('.sortable').sortable();
+});
 
 
 
